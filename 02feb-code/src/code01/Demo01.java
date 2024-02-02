9e0c93caa632b7c4229c9e66b3b85ad38968ab6d
18292872556 <18292872556@163.com>
2024-02-02 23:08:24 +0800

    add 02feb-code file

---
+++ 02feb-code/src/code01/Demo01.java
@@ -0,0 +1,17 @@
+package code01;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 雪雪子
+ * @Date: 2024/02/02/18:34
+ * @Description:
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        Zi zi = new Zi();
+        zi.show();
+        zi.method();
+    }
+
+}
---
+++ 02feb-code/src/code01/Fu.java
@@ -0,0 +1,15 @@
+package code01;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: 雪雪子
+ * @Date: 2024/02/02/18:39
+ * @Description:
+ */
+public class Fu {
+    int num = 10;
+    void method(){
+        System.out.println("Fu的method方法");
+    }
+}
