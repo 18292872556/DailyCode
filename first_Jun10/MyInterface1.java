bfb54d7bc099a4de10c85ebd14ce7592f8b2f278
18292872556 <18292872556@163.com>
2026-06-11 23:16:20 +0800

    add first_Jun10 file

---
+++ first_Jun10/MyInterface1.java
@@ -0,0 +1,15 @@
+package Advanced.Day02_Collection_泛型.first_Jun10;
+
+/**
+ * Created with IntelliJ IDEA 2024.1.4.
+ * jdk 17.0.12
+ *
+ * @Authur: xuexuezi
+ * @Date: 2026/06/11/下午5:22
+ * @Description:
+ */
+public interface MyInterface1<E>{
+    //public static final E num = null;
+    public abstract void add(E e);
+    E getE();
+}
---
+++ first_Jun10/MyInterface1Impl1.java
@@ -0,0 +1,20 @@
+package Advanced.Day02_Collection_泛型.first_Jun10;
+
+/**
+ * Created with IntelliJ IDEA 2024.1.4.
+ * jdk 17.0.12
+ *
+ * @Authur: xuexuezi
+ * @Date: 2026/06/11/下午6:39
+ * @Description:
+ */
+public class MyInterface1Impl1 implements MyInterface1<String>{
+    @Override
+    public void add(String e){
+
+    }
+    @Override
+    public String getE(){
+        return null;
+    }
+}
